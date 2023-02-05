e577cfa78575c498c2320922109cabc00bf5e35d
Qu1etboy <nonzagreanthai@gmail.com>
2023-02-05 23:41:31 +0700

    Make rename.sh more dynamic

--- rename.sh
+++ rename.sh
@@ -1,12 +1,15 @@
+# How to use
+# bash rename.sh <folder name that contain list of files> <index> <name>
+
 search_dir=$1
-i=72
-zone='WM'
+i=$2
+name=$3
 for entry in "$search_dir"/*
 do
-  mv "$entry" "${search_dir}/${zone}${i}.jpg"
+  mv "$entry" "${search_dir}/${name}${i}.jpg"
   i=$((i+1))
   # echo "$entry"
 done
 
-# "./files/${zone}${i}.txt"
-# "./files/${zone}${i}-$((i+1)).txt"
+# "./files/${name}${i}.txt"
+# "./files/${name}${i}-$((i+1)).txt"
